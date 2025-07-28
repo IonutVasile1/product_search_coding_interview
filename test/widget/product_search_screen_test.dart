f300d518940ce88160509e93960d43a9c71d6dd5
IonutVasile1 <ionut.vasile@awin.com>
2025-07-28 15:02:41 +0300

    fix one failing test

--- test/widget/product_search_screen_test.dart
+++ test/widget/product_search_screen_test.dart
@@ -3,17 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:product_search_coding_interview/main.dart';
 
 void main() {
-  testWidgets('App starts with Product Search screen', (WidgetTester tester) async {
+  testWidgets('App starts with Product Search screen', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(const MyApp());
 
     // Verify the app shows the Product Search screen
     expect(find.text('Product Search'), findsOneWidget);
     expect(find.byType(TextField), findsOneWidget);
-    expect(find.text('Search for products'), findsOneWidget);
+    expect(find.text('Start typing to search for products'), findsOneWidget);
   });
 
-  
-  testWidgets('Search produces results for valid query', (WidgetTester tester) async {
+  testWidgets('Search produces results for valid query', (
+    WidgetTester tester,
+  ) async {
     // TODO: write a test to verify that searching for a product produces results when a valid query is entered
     // you can use "Laptop" as a valid query
   });
